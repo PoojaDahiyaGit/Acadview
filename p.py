22589007ecdaf1a8b46cb787296ac373d1df2afc
PoojaDahiyaGit <poojadahiya.pd1994@gmail.com>
2018-08-09 17:38:23 +0530

    First commit

---
+++ p.py
@@ -0,0 +1,60 @@
+Python 3.5.1 (v3.5.1:37a07cee5969, Dec  6 2015, 01:38:48) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 3 or 4
+3
+>>> 4 or 3
+4
+>>> 4 or -1
+4
+>>> -1 or 4
+-1
+>>> 5 or "titu"
+5
+>>> "titu" or 5
+'titu'
+>>> "sweety" or "sonu"
+'sweety'
+>>> "sonu" or -1
+'sonu'
+>>> 3 and 4
+4
+>>> 4 and 3
+3
+>>> 4 and -1
+-1
+>>> -1 and 4
+4
+>>> "sonu"and -1
+-1
+>>> -1 and "titu"
+'titu'
+>>> True and 3
+3
+>>> True or 3
+True
+>>> 3 or True
+3
+>>> False or True
+True
+>>> False or 5
+5
+>>> 0 or 6
+6
+>>> -1 or 5
+-1
+>>> True or 78
+True
+>>> 
+>>> 
+>>> 
+>>> False or False
+False
+>>> False and 5
+False
+>>> True and False
+False
+>>> 5 and False
+False
+>>> False and 9
+False
+>>> 
